fcf0db55f31449f86686199cd08fb0566ee42fb1
wd520conan <30793949+wd520conan@users.noreply.github.com>
2020-10-29 14:14:55 +0800

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -1,5 +1,5 @@
 // 注意：live2d_path 参数应使用绝对路径
-const live2d_path = "https://cdn.jsdelivr.net/gh/wd520conan/live2d-widget@1.4/";
+const live2d_path = "https://cdn.jsdelivr.net/gh/wd520conan/live2d-widget@1.5/";
 //const live2d_path = "/live2d-widget/";
 
 // 封装异步加载资源的方法
